28c4e800a3e261b47ea9715cef3c7c6a58a1d7ed
LesCharlie <carlos.sanchez3875@alumnos.udg.mx>
2025-06-25 12:56:50 -0600

    Primer versión del sitio de Ferretería Central

---
+++ script.js
@@ -0,0 +1,35 @@
+function generarLetrasAnimadas(contenedorId) {
+  const contenedor = document.getElementById(contenedorId);
+  const total = 50; // Puedes cambiar el número de textos
+
+  for (let i = 0; i < total; i++) {
+    const span = document.createElement('span');
+    span.classList.add('texto-animado');
+    span.classList.add(i % 2 === 0 ? 'subir' : 'bajar');
+    span.textContent = 'Ferretería Central';
+
+    // Posiciones aleatorias
+    const top = Math.random() * 100;
+    const left = Math.random() * 100;
+
+    span.style.top = `${top}%`;
+    span.style.left = `${left}%`;
+
+    contenedor.appendChild(span);
+  }
+}
+
+// Ejecutar al cargar
+document.addEventListener('DOMContentLoaded', () => {
+  generarLetrasAnimadas('letras-header');
+  generarLetrasAnimadas('letras-footer');
+});
+
+function moverCarrusel(direccion) {
+  const contenedor = document.getElementById("contenedorCategorias");
+  const anchoElemento = contenedor.querySelector(".categoria").offsetWidth + 16; // incluye gap
+  contenedor.scrollBy({
+    left: direccion * anchoElemento * 2, // mueve 2 tarjetas por clic
+    behavior: "smooth"
+  });
+}
